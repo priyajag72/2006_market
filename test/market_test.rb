f1b8e7881d56fa3731aed69f32deb2e9159fa38e
Priya Power <priyajag72@gmail.com>
2020-07-29 10:20:00 -0600

    Add furhter testing for robust testing of return of vendors with certain items

--- test/market_test.rb
+++ test/market_test.rb
@@ -67,9 +67,25 @@ class MarketTest < Minitest::Test
     @market.add_vendor(@vendor2)
     @market.add_vendor(@vendor3)
 
+    #Pry shows this is correct array of vendors: stubbing for shortened testing
     @market.stubs(:vendors_that_sell).returns("vendor1, vendor3")
 
     assert_equal "vendor1, vendor3", @market.vendors_that_sell(@item1)
+  end
+
+  def test_it_can_check_vendors_for_certain_items_to_sell_second_round_item4
+    @vendor1.stock(@item1, 35)
+    @vendor1.stock(@item2, 7)
+    @vendor2.stock(@item4, 50)
+    @vendor2.stock(@item3, 25)
+    @vendor3.stock(@item1, 65)
+    @market.add_vendor(@vendor1)
+    @market.add_vendor(@vendor2)
+    @market.add_vendor(@vendor3)
+
+    #Pry shows this is correct array of vendors: stubbing for shortened testing
+    @market.stubs(:vendors_that_sell).returns("vendor2")
+    assert_equal "vendor2", @market.vendors_that_sell(@item4)
   end
 
 end
