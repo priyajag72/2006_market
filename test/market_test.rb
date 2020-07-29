4c69972e962fb829bc9c8f0862192ce50586c258
Priya Power <priyajag72@gmail.com>
2020-07-29 09:53:10 -0600

    Create market test file; Add minitest syntax; Add setup with 4 items, 3 vendors, 1 market; Add test it exists

---
+++ test/market_test.rb
@@ -0,0 +1,29 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/item"
+require "./lib/vendor"
+require "./lib/market"
+
+class MarketTest < Minitest::Test
+
+  def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: "$0.50"})
+    @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+    @vendor1 = Vendor.new("Rocky Mountain Fresh")
+    @vendor2 = Vendor.new("Ba-Nom-a-Nom")
+    @vendor3 = Vendor.new("Palisade Peach Shack")
+
+    @market = Market.new("South Pearl Street Farmers Market")
+  end
+
+  def test_it_exists
+    assert_instance_of Market, @market
+  end
+
+  #def test_it_has_attributes
+  #end
+
+end
