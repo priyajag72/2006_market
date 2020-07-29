1ad18f8ac4482dff3412c6ace023edfc1904ebf3
Priya Power <priyajag72@gmail.com>
2020-07-29 09:12:06 -0600

    Add test_it_can_check_stock with correct assertions and behaviors; Updates setup for item objects

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -6,6 +6,9 @@ require "./lib/vendor"
 class VendorTest < Minitest::Test
 
   def setup
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
     @vendor = Vendor.new("Rocky Mountain Fresh")
   end
 
@@ -16,6 +19,13 @@ class VendorTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Rocky Mountain Fresh", @vendor.name
     assert_equal ({}), @vendor.inventory
+  end
+
+  def test_it_can_check_stock
+    assert_equal 0, @vendor.check_stock(@item1)
+    # @vendor.stock(item1, 30)
+    # expected = d
+    # assert_equal expected, @vendor.inventory
   end
 
 end
