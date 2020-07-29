e235fd0c6df9b9f047e4054146a62b9842608b62
Priya Power <priyajag72@gmail.com>
2020-07-29 09:01:26 -0600

    Add test it has attributes for attribute name and price to Item

--- test/item_test.rb
+++ test/item_test.rb
@@ -14,7 +14,9 @@ class ItemTest < Minitest::Test
     assert_instance_of Item, @item2
   end
 
-  #def test_it_has_attributes
-  #end
+  def test_it_has_attributes
+    assert_equal "Tomato", @item2.name
+    assert_equal 0.5, @item2.price
+  end
 
 end
