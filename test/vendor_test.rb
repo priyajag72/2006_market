24f3427e59eb20feb1e403a74f91507dc1ef755e
Priya Power <priyajag72@gmail.com>
2020-07-29 09:06:48 -0600

    Add minitest sytax, setup with 1 object, and test it exists to Vendor

---
+++ test/vendor_test.rb
@@ -0,0 +1,19 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/item"
+require "./lib/vendor"
+
+class VendorTest < Minitest::Test
+
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  #def test_it_has_attributes
+  #end
+
+end
